879e68752e19f190e84d62eb073b5d6657b2367d
An Nguyen <19521185@gm.uit.edu.vn>
2023-03-03 21:08:53 +0700

    Initialize UITBooks-Frontend

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://uitbooks-be.vercel.app/";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
